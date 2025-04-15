986e46d830380b885a846ac2699f73dbd027dfa0
FZH010618 <139457663+FZH010618@users.noreply.github.com>
2025-04-15 19:18:43 +0800

    Update eventType.ts

--- studio/schemaTypes/eventType.ts
+++ studio/schemaTypes/eventType.ts
@@ -51,20 +51,31 @@ export const eventType = defineType({
   ],
 })
 
-// Replace "slug" in the array of fields:
-defineField({
-  name: 'slug',
-  type: 'slug',
-  options: {source: 'name'},
-}),
-
-
-  // Replace "eventType" in the array of fields:
-defineField({
-  name: 'eventType',
-  type: 'string',
-  options: {
-    list: ['in-person', 'virtual'],
-    layout: 'radio',
+// Update the preview key in the schema
+preview: {
+  select: {
+    name: 'name',
+    venue: 'venue.name',
+    artist: 'headline.name',
+    date: 'date',
+    image: 'image',
   },
-}),
+  prepare({name, venue, artist, date, image}) {
+    const nameFormatted = name || 'Untitled event'
+    const dateFormatted = date
+      ? new Date(date).toLocaleDateString(undefined, {
+          month: 'short',
+          day: 'numeric',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+        })
+      : 'No date'
+
+    return {
+      title: artist ? `${nameFormatted} (${artist})` : nameFormatted,
+      subtitle: venue ? `${dateFormatted} at ${venue}` : dateFormatted,
+      media: image || CalendarIcon,
+    }
+  },
+},
