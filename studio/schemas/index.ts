9e61adaaa71a7b985c6faf49fc6d14f1bce7c822
FZH010618 <139457663+FZH010618@users.noreply.github.com>
2025-04-15 16:33:01 +0800

    Update index.ts

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,5 +1,5 @@
-// studio/schemaTypes/index.ts
-import group from '../schemas/group';
-import listing from '../schemas/listing'; // 如果你还有别的 schema，一起引入
+import group from './group'
+import listing from './listing'
 
-export const schemaTypes = [group, listing];
+export const schemaTypes = [group, listing]
+ 
